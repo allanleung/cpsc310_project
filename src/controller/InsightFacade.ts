4837fdf1d3527189447b7f9d9bcd209be6d31cee
Jnani Weibel <jnaneshwar1234@gmail.com>
2017-01-19 11:43:38 -0800

    addDataset initial impl

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -2,17 +2,46 @@
  * This is the main programmatic entry point for the project.
  */
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
+import JSZip from 'jszip';
 
 import Log from "../Util";
 
 export default class InsightFacade implements IInsightFacade {
+    dataSet: Map<string, any>;
 
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
+        this.dataSet = new Map<string, any>();
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+        return new Promise<InsightResponse>((resolve, reject) => {
+            JSZip.loadAsyc(content)
+            .then((zip) => {
+                return zip.file(id).async("string");
+            })
+            .then((data) => {
+                let statusCode = 204;
+                if (dataSet.has(id)) {
+                    statusCode = 201
+                }
+
+                dataSet.set(id, JSON.parse(data));
+
+                resolve({
+                    code: statusCode,
+                    body: {}
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    code: 400,
+                    body: {
+                        error: err
+                    }
+                });
+            });
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
