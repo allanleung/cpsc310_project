bdb48c53825b368334cf9a9a9538a42c0451198e
Jnani Weibel <jnaneshwar1234@gmail.com>
2017-01-26 11:57:32 -0800

    Transform and add tests

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -2,7 +2,8 @@
  * This is the main programmatic entry point for the project.
  */
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
-import JSZip from 'jszip';
+import * as JSZip from 'jszip' ;
+import util = require('util');
 
 import Log from "../Util";
 
@@ -14,27 +15,53 @@ export default class InsightFacade implements IInsightFacade {
         this.dataSet = new Map<string, any>();
     }
 
+/**
+courses_dept: string; The department that offered the course.
+courses_id: string; The course number (will be treated as a string (e.g., 499b)).
+courses_avg: number; The average of the course offering.
+courses_instructor: string; The instructor teaching the course offering.
+courses_title: string; The name of the course.
+courses_pass: number; The number of students that passed the course offering.
+courses_fail: number; The number of students that failed the course offering.
+courses_audit: number; The number of students that audited the course offering.
+courses_uuid: string; The unique id of a course offering
+*/
+
     addDataset(id: string, content: string): Promise<InsightResponse> {
         return new Promise<InsightResponse>((resolve, reject) => {
-            JSZip.loadAsyc(content)
-            .then((zip) => {
+            new JSZip().loadAsync(content)
+            .then((zip: any) => {
                 return zip.file(id).async("string");
             })
-            .then((data) => {
+            .then((data: any) => {
                 let statusCode = 204;
                 if (this.dataSet.has(id)) {
                     statusCode = 201
                 }
 
-                this.dataSet.set(id, JSON.parse(data));
+                let result = JSON.parse(data).result.map((entry: any) => {
+                    return {
+                        courses_dept: entry.Subject,
+                        courses_id: entry.Course,
+                        courses_avg: entry.Avg,
+                        courses_instructor: entry.Professor,
+                        courses_title: entry.Title,
+                        courses_pass: entry.Pass,
+                        courses_fail: entry.Fail,
+                        courses_audit: entry.Audit,
+                        courses_uuid: entry.id
+                    };
+                })
+
+                this.dataSet.set(id, result);
 
                 resolve({
                     code: statusCode,
                     body: {}
                 });
             })
-            .catch((err) => {
-                resolve({
+            .catch((err: any) => {
+                reject({
                     code: 400,
                     body: {
                         error: err
--- test/InsightFacadeSpec.ts
+++ test/InsightFacadeSpec.ts
@@ -1,16 +1,94 @@
 /**
- * Created by jerome on 2017-01-19.
- */
+* Created by jerome on 2017-01-19.
+*/
 
 import InsightFacade from "../src/controller/InsightFacade";
+var rp = require('request-promise-native');
 import {expect} from 'chai';
+import * as mocha from 'mocha';
+
+describe("InsightFacade.addDataset", () => {
+    var insightFacade: InsightFacade = null;
+    let content: string;
+
+    before(() => {
+        return rp({
+            uri: 'https://github.com/ubccpsc/310/blob/2017jan/project/courses.zip?raw=true',
+            encoding: null
+        }).then((response: any) => {
+            content = response;
+        });
+    });
+
+    beforeEach(() => {
+        insightFacade = new InsightFacade();
+    });
+
+    afterEach(() => {
+        insightFacade = null;
+    });
+
+    it('should add an id to the dataset successfully', (done) => {
+        insightFacade.addDataset("courses/VISA110", content)
+        .then((response) => {
+            expect(response).to.deep.eq({
+                code: 204,
+                body: {}
+            });
+
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('should add an id to the dataset successfully twice', (done) => {
+        (function (insightFacade) {
+            insightFacade.addDataset("courses/VISA110", content).then((response) => {
+                expect(response).to.deep.eq({
+                    code: 204,
+                    body: {}
+                });
+
+                return insightFacade.addDataset("courses/VISA110", content);
+            }).then((response) => {
+                expect(response).to.deep.eq({
+                    code: 201,
+                    body: {}
+                });
+
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        })(insightFacade);
+    });
+
+    it('should fail to add an invalid id', (done) => {
+        (function (insightFacade) {
+            insightFacade.addDataset("courses/invalid", content).then((response) => {
+                done(new Error('there should not be a response'));
+            }).catch((err) => {
+                expect(err).to.deep.eq({
+                    code: 400,
+                    body: {
+                        error: {}
+                    }
+                });
+
+                done();
+            });
+        })(insightFacade);
+    });
+
+});
 
 describe("InsightFacade.removeDataset", () => {
     let insightFacade: InsightFacade = null;
 
     beforeEach(() => {
         insightFacade = new InsightFacade();
-        insightFacade.dataSet.set("VISA110", {});
+        insightFacade.dataSet.set("courses/VISA110", {});
     });
 
     afterEach(() => {
@@ -18,7 +96,7 @@ describe("InsightFacade.removeDataset", () => {
     });
 
     it('should remove an existing ID successfully', () => {
-        insightFacade.removeDataset("VISA110").then((response) => {
+        insightFacade.removeDataset("courses/VISA110").then((response) => {
             expect(response).to.deep.eq({
                 code: 200,
                 body: {}
