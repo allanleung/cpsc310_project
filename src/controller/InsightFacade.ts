121edd609dfb21cba5453389f75094c67fd7219c
Allan Leung <allanleung1018@gmail.com>
2017-02-03 22:29:31 -0800

    added 1 little test, still working progress

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -164,12 +164,11 @@ export default class InsightFacade implements IInsightFacade {
         return new Promise<InsightResponse>((fulfill, reject) => {
             let queryList : any [] = [];
             this.dataSet.forEach((value, key) => {
-                value.forEach((value2) => {
-                    this.compareQuery(query, value2);
-                    if (this.compareQuery(query, value2)) {
-                        queryList.push(value2);
-                    }
-
+                    value.forEach((value2) => {
+                        this.compareQuery(query, value2);
+                        if (this.compareQuery(query, value2)) {
+                            queryList.push(value2);
+                        }
                     })
                 }
 
@@ -188,11 +187,11 @@ export default class InsightFacade implements IInsightFacade {
                 }
             })
             queryList.forEach((value) => {
-              for (let key of Object.keys(value)) {
-                  if(query.OPTIONS.COLUMNS.indexOf(key) === -1)  {
-                      delete value[key];
-                  }
-              }
+                for (let key of Object.keys(value)) {
+                    if(query.OPTIONS.COLUMNS.indexOf(key) === -1)  {
+                        delete value[key];
+                    }
+                }
             })
             fulfill({
                 code: 200,
@@ -202,17 +201,6 @@ export default class InsightFacade implements IInsightFacade {
                 }
             });
 
-            /**
-             courses_dept: string; The department that offered the course.
-             courses_id: string; The course number (will be treated as a string (e.g., 499b)).
-             courses_avg: number; The average of the course offering.
-             courses_instructor: string; The instructor teaching the course offering.
-             courses_title: string; The name of the course.
-             courses_pass: number; The number of students that passed the course offering.
-             courses_fail: number; The number of students that failed the course offering.
-             courses_audit: number; The number of students that audited the course offering.
-             courses_uuid: string; The unique id of a course offering
-             */
         });
     }
 }
