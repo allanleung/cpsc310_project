8c61a49d120044fce21f40309a909af8a1a60d0a
Jnani Weibel <jnaneshwar1234@gmail.com>
2017-03-15 18:24:40 -0700

    Basic column selection

--- src/rest/public/app/app.module.js
+++ src/rest/public/app/app.module.js
@@ -1,4 +1,9 @@
 /**
  * Created by Jnani on 3/15/17.
  */
-var app = angular.module('app', []);
+var app = angular.module('app', []).run(($rootScope) => {
+    //Just add a reference to some utility methods in rootscope.
+    $rootScope.Utils = {
+        keys: Object.keys
+    };
+});
--- src/rest/public/app/results.component.js
+++ src/rest/public/app/results.component.js
@@ -1,66 +1,81 @@
 angular.module('app').component('results', {
     template: `
+<ul class="unstyled">
+    <li ng-repeat="column in $root.Utils.keys($ctrl.columns)">
+        <label class="checkbox">
+            <input type="checkbox" ng-model="$ctrl.columns[column]">
+            <span>{{column}}</span>
+        </label>
+    </li>
+</ul>
+
+<a href="" ng-click="$ctrl.query()">Query</a>
+
 <table class="table table-hover">
     <thead>
     <tr>
-        <th ng-repeat="key in $ctrl.keys">{{key}}</th>
+        <th ng-repeat="key in $root.Utils.keys($ctrl.columns)" ng-if="$ctrl.columns[key]">{{key}}</th>
     </tr>
     </thead>
     <tbody>
     <tr ng-repeat="result in $ctrl.results">
-        <th ng-repeat="key in $ctrl.keys">{{result[key]}}</th>
+        <th ng-repeat="key in $root.Utils.keys($ctrl.columns)" ng-if="$ctrl.columns[key]">{{result[key]}}</th>
     </tr>
     </tbody>
 </table>
-`, controller: function ResultListController($scope, $http) {
-        this.keys = [
-            "courses_dept",
-            "courses_id",
-            "courses_avg",
-            "courses_instructor",
-            "courses_title",
-            "courses_pass",
-            "courses_fail",
-            "courses_audit",
-            "courses_uuid",
-            "courses_year"
-        ];
+`, controller: function QueryController($scope, $http) {
+        this.columns = {
+            "courses_dept": true,
+            "courses_id": true,
+            "courses_avg": true,
+            "courses_instructor": true,
+            "courses_title": true,
+            "courses_pass": true,
+            "courses_fail": true,
+            "courses_audit": true,
+            "courses_uuid": true,
+            "courses_year": true
+        };
 
-        $http.post('/query', {
-            "WHERE": {
-                "OR":[
-                    {
-                        "AND":[
-                            {
-                                "GT":{
-                                    "courses_avg":90
+        this.query = function() {
+            $http.post('/query', {
+                "WHERE": {
+                    "OR":[
+                        {
+                            "AND":[
+                                {
+                                    "GT":{
+                                        "courses_avg":90
+                                    }
+                                },
+                                {
+                                    "IS":{
+                                        "courses_dept":"adhe"
+                                    }
                                 }
-                            },
-                            {
-                                "IS":{
-                                    "courses_dept":"adhe"
-                                }
+                            ]
+                        },
+                        {
+                            "EQ":{
+                                "courses_avg":95
                             }
-                        ]
-                    },
-                    {
-                        "EQ":{
-                            "courses_avg":95
                         }
-                    }
-                ]
-            },
-            "OPTIONS": {
-                "COLUMNS": this.keys,
-                "ORDER": {
-                    "dir": "UP",
-                    "keys": ["courses_dept", "courses_id"]
+                    ]
                 },
-                "FORM": "TABLE"
-            }
-        }, {}).then((response) => {
-            console.log(response);
-            this.results = response.data.result;
-        });
+                "OPTIONS": {
+                    "COLUMNS": Object.keys(this.columns).filter(e => {
+                        return this.columns[e];
+                    }),
+                    "ORDER": {
+                        "dir": "UP",
+                        "keys": ["courses_dept", "courses_id"]
+                    },
+                    "FORM": "TABLE"
+                }
+            }, {}).then((response) => {
+                this.results = [];
+                this.results = response.data.result;
+            });
+        };
     }
 });
